753b235f8d9126d88a83cc11f68eead9b5a751a4
gobbyboss <gobbyboss@gmail.com>
2023-12-11 22:30:32 -0700

    Added DND styled rolling functionality
    Added support for DND styled rolling for a command like "~roll 2d20". This will roll 2 twenty sided die.
    
    Also added size limitation on individual rolls.

--- src/Roll.py
+++ src/Roll.py
@@ -1,19 +1,37 @@
-import random
+import random, re
 from datetime import datetime
 class Roll():
-    def __init__(self, args):
-        self.args = args
+    def __init__(self):
         self.low = 1
-        self.high = 20
+        self.high = 21
         self.rolls = 1
         self.results = []
         self.validArgs = True
-        self.checkArgs()
 
-    def checkArgs(self):
-        if len(self.args) != 0:
-            self.validArgs = False #TODO: Check either 1d20 stye format or low, high, and numrolls(optional) for args. Can call this check in Main and intercept roll() logic
-    
+    def setRollCfg(self, args):
+        self.args = args
+        if len(self.args) == 0:
+            return
+        elif len(self.args) == 1:
+            mod = self.args[0]
+            searchString = "^\d+[d]\d+"
+            valid = re.search(searchString, mod)
+            if not valid:
+                return -1
+            dIndex = 0
+            modChar = ''
+            while modChar != 'd':
+                modChar = mod[dIndex]
+                if(modChar != 'd'):
+                    dIndex += 1
+            self.low = 1
+            self.rolls = int(mod[0:dIndex])
+            self.high = int(mod[dIndex+1:len(mod)]) + 1
+            if self.rolls > 100 or self.high > 999999999999999:
+                return -1
+        else:
+            return
+
     def roll(self):
         if not self.validArgs:
             return -1
