7f8e9a115415fdc5ded1defd2e00305a5e0bcb1c
gobbyboss <gobbyboss@gmail.com>
2026-06-13 15:14:16 -0600

    Added queue features

--- src/Music.py
+++ src/Music.py
@@ -9,11 +9,14 @@ PROGRAM_NAME = "Music.py"
 #                                                        #
 ##########################################################
 
-import ffmpeg, discord, yt_dlp, re
+import discord, yt_dlp, re, Utils, asyncio
+from datetime import datetime, date
 class Music():
     def __init__(self, args, ctx):
         self.ctx = ctx
         self.args = args
+        self.con = Utils.ConnectDB()
+        self.cursor = self.con.cursor()
 
     async def play(self, youtube):
         if not self.ctx.author.voice:
@@ -32,25 +35,55 @@ class Music():
         if not isUrl:
             ytUrl = self.ytSearch(youtube, songName)
             if ytUrl is None:
-                msg = "Failed to play " + songName
+                msg = "Failed to queue " + songName
                 await self.ctx.send(msg)
                 return
         else:
             ytUrl = self.checkValidUrl(songName)
-            print(ytUrl)
             if ytUrl is None:
                 msg = "Invalid URL, please use a valid YouTube url. Example:\n`~play https://www.youtube.com/watch?v=6hzrDeceEKc`"
                 await self.ctx.send(msg)
                 return
 
-        print(ytUrl)
-        musicFile = self.ytDownload(ytUrl)
-        print(musicFile)
-        played = await self.playMusic(musicFile)
-        if not played:
-            msg = "Failed to play song"
-            await self.ctx.send(msg)
+        if self.ctx.voice_client and self.ctx.voice_client.is_playing():
+            self.insertQueue(ytUrl)
             return
+     
+        queueEmpty = False
+        inQueue = False
+        while queueEmpty is False:
+            musicFile = await asyncio.get_event_loop().run_in_executor(None, self.ytDownload, ytUrl)
+            if musicFile is None:
+                msg = "Song retrieval failed. Please try again later"
+                self.ctx.send(msg)
+
+            played = await self.playMusic(musicFile)
+            if not played:
+                msg = "Failed to play song"
+                await self.ctx.send(msg)
+                return
+
+            queueEmpty = self.checkQueue()
+            if inQueue:
+                res = self.deleteQueue(queueId)
+                if res is None:
+                    msg = "Failed to remove song from queue"
+                    await self.ctx.send(msg)
+                    return
+            if queueEmpty:
+                return
+            else:
+                queueId = self.getQueueId()
+                if queueId is None:
+                    msg = "Failed to retrieve song from queue"
+                    await self.ctx.send(msg)
+                    return
+                ytUrl = self.getQueueUrl(queueId)
+                if ytUrl is None:
+                    msg = "Failed to retrieve song from queue"
+                    await self.ctx.send(msg)
+                    return
+
 
     def checkYtUrl(self, songName):
         pattern = r"(youtube\.com|youtu\.be)"
@@ -105,6 +138,8 @@ class Music():
                 options="-vn"
             )
             connection.play(source)
+            while connection.is_playing():
+                await asyncio.sleep(1)
             return True
         except Exception as e:
             return False
@@ -120,6 +155,70 @@ class Music():
                 'preferredcodec': 'mp3',
             }],
         }
-        with yt_dlp.YoutubeDL(opts) as ydl:
-            info = ydl.extract_info(url, download=True)
-            return f"../data/ytCache/{info['id']}.mp3"
+        try:
+            with yt_dlp.YoutubeDL(opts) as ydl:
+                info = ydl.extract_info(url, download=True)
+                return f"../data/ytCache/{info['id']}.mp3"
+        except Exception as e:
+            return None
+    
+    def checkQueue(self):
+        guildId = self.ctx.guild.id
+        query = 'SELECT COUNT(*) FROM T_QUEUE WHERE GUILD_ID = ?'
+        try:
+            result = self.cursor.execute(query, (guildId,)).fetchone()
+        except Exception as e:
+            msg = "Error in checkQueue executing:\n" + query
+            Utils.logError(msg, PROGRAM_NAME, str(e))
+            return
+        count = result[0]
+        return count == 0
+
+    def insertQueue(self, url):
+        guildId = self.ctx.guild.id
+        self.setTime()
+        
+        query = """
+                  INSERT INTO T_QUEUE VALUES(%s,%s,%s,%s)
+                """ % (guildId, url, self.day, self.time)
+        try:
+            self.cursor.execute(query)
+        except Exception as e:
+            msg = "Error in insertQueue executing:\n" + query
+            Utils.logError(msg, PROGRAM_NAME, str(e))
+            return
+        self.con.commit()
+    
+    def setTime(self):
+        today = date.today()
+        now = datetime.now()
+
+        self.day = str(today.strftime("%Y%m%d"))
+        self.time = str(now.strftime("%H%M%S"))
+
+    def getQueueId(self):
+        guildId = self.ctx.guild.id
+        query = 'SELECT ID FROM T_QUEUE WHERE GUILD_ID = ? ORDER BY ROWID ASC LIMIT 1'
+        result = self.cursor.execute(query, (guildId,)).fetchone()
+        if result:
+            return result[0]
+        return None
+
+    def getQueueUrl(self, queueId):
+        query = 'SELECT URL FROM T_QUEUE WHERE ID = ?'
+        result = self.cursor.execute(query, (queueId,)).fetchone()
+        if result:
+            return result[0]
+        return None
+    
+    def deleteQueue(self, queueId):
+        query = 'DELETE FROM T_QUEUE WHERE ID = ?'
+        try:
+            self.cursor.execute(query, (queueId,))
+        except Exception as e:
+            msg = "Error in deleteQueue executing:\n" + query
+            Utils.logError(msg, PROGRAM_NAME, str(e))
+            return
+        self.con.commit()
+
+        
